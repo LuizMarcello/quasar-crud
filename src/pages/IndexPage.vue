e474fc97ec187729e13ff20ee963721a9fc98dd9
luizmarcello <luizmarcello.vmo@hotmail.com>
2023-03-28 17:49:57 -0300

    Rolândia:28.03.23_17:49hs

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,49 @@
+<!-- eslint-disable -->
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <q-table title="Treats" :rows="posts" :columns="columns" row-key="name" />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+/* eslint-disable */
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+
+  /* Criando as lógicas do componente */
+  setup() {
+    const posts = ref([]);
+
+    const columns = [
+      {
+        name: "id",
+        field: "id",
+        label: "Id",
+        sortable: true,
+        align: 'left'
+      },
+      {
+        name: "title",
+        field: "title",
+        label: "Título",
+        sortable: true,
+        align: 'left'
+      },
+      {
+        name: "author",
+        field: "author",
+        label: "Autor",
+        sortable: true,
+        align: 'left'
+      },
+    ];
+
+    return {
+      posts,
+      columns,
+    };
+  },
+});
 </script>
